731679856a56a608024e8e922c1e6f824793910d
Cameron Buscher <cameronbuscher@gmail.com>
2018-11-28 20:58:25 +0000

    Add continue functionality with custom function

--- js/quiz.js
+++ js/quiz.js
@@ -39,5 +39,9 @@
         $scope.isCorrect = function (qIndex, aIndex) {
             return $scope.myQuestions[qIndex].correctAnswer === aIndex
         }
+        
+        $scope.selectContinue = function() {
+            return $scope.activeQuestion += 1
+        }
     }]);
 })();
